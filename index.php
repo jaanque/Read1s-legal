08bbedcff584f112396b69d9f52378e03efa6b70
jan <jan@ilerda.com>
2025-03-13 02:04:50 +0100

    UPDATE // css

--- index.php
+++ index.php
@@ -6,15 +6,16 @@ include 'header-legal.php'; ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.php.css">
     <title>Read1s Legal</title>
 </head>
 <body>
     <h1>Legal Pages</h1>
     <ul>
-        <li><a href="Terms-of-Service.php">Terms of Service</a></li>
-        <li><a href="Cookie-Policy.php">Cookie Policy</a></li>
-        <li><a href="Privacy-Policy.php">Privacy Policy</a></li>
-        <li><a href="Legal-Notice.php">Legal Notice</a></li>
+        <li class="li"><a href="Terms-of-Service.php">Terms of Service</a></li>
+        <li class="li"><a href="Cookie-Policy.php">Cookie Policy</a></li>
+        <li class="li"><a href="Privacy-Policy.php">Privacy Policy</a></li>
+        <li class="li"><a href="Legal-Notice.php">Legal Notice</a></li>
     </ul>
 </body>
 </html>
