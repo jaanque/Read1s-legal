010f39914bdf3620c412f2a8d563eeecb948d767
jan <jan@ilerda.com>
2025-03-13 02:28:08 +0100

    updating...

--- index.php
+++ index.php
@@ -10,12 +10,14 @@ include 'header-legal.php'; ?>
     <title>Read1s Legal</title>
 </head>
 <body>
-    <h1>Legal Pages</h1>
-    <ul>
-        <li class="li"><a href="Terms-of-Service.php">Terms of Service</a></li>
-        <li class="li"><a href="Cookie-Policy.php">Cookie Policy</a></li>
-        <li class="li"><a href="Privacy-Policy.php">Privacy Policy</a></li>
-        <li class="li"><a href="Legal-Notice.php">Legal Notice</a></li>
+    <br><br><br>
+    <h1 class="h1">Legal Pages</h1>
+    <br>
+    <ul class="ul">
+        <li class="li"><a class="a" href="Terms-of-Service.php">Terms of Service</a></li>
+        <li class="li"><a class="a" href="Cookie-Policy.php">Cookie Policy</a></li>
+        <li class="li"><a class="a" href="Privacy-Policy.php">Privacy Policy</a></li>
+        <li class="li"><a class="a" href="Legal-Notice.php">Legal Notice</a></li>
     </ul>
 </body>
 </html>
